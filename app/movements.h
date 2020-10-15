029b1be47c5215de92181957901422657a9208c1
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2020-10-15 14:51:02 +0300

    v0.51
    Добавлена функция defaultSword

--- app/movements.h
+++ app/movements.h
@@ -2,3 +2,5 @@ void moveUp(int*, int*, int*, int*, char**, char);
 void moveDown(int*, int*, int*, int*, char**, char);
 void moveRight(int*, int*, int*, int*, char**, char);
 void moveLeft(int*, int*, int*, int*, char**, char);
+void doNothing(void);
+void defaultSword(int* x, int* y, int* health, int* score, char** map);
