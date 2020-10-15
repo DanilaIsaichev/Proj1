33d9eba7ca8c92eb082646b8386d061ffdc65497
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2020-10-15 07:42:01 +0300

    Create movements.h

---
+++ movements.h
@@ -0,0 +1,4 @@
+void moveUp(int*, int*, int*, int*, char**, char);
+void moveDown(int*, int*, int*, int*, char**, char);
+void moveRight(int*, int*, int*, int*, char**, char);
+void moveLeft(int*, int*, int*, int*, char**, char);
